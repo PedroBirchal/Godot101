8c09fb4ccfa9e46473d883362aefe92f93f220c3
Pedro de Lima Birchal (Lima) <birchalpedro@gmail.com>
2025-06-08 23:03:56 -0300

    Auto stash before merge of "main" and "origin/main"

--- Scripts/LimaGambler.gd
+++ Scripts/LimaGambler.gd
@@ -1,4 +1,28 @@
 extends CharacterBody2D
 
-func _ready() -> void:
-	print('Hello, World!')
+
+const gravity : float = 9.8
+@export var gravity_multiplier : float = 15
+@export var jump_force : float = 100
+
+var max_rotation : float = -PI * 0.4
+var min_rotation : float = PI * 0.4
+var velocity_cap : float = 100.0
+
+
+var current_velocity : Vector2 = Vector2.ZERO
+
+
+func _physics_process(delta: float) -> void:
+	current_velocity.y +=  gravity * delta * gravity_multiplier
+	
+	if(Input.is_action_just_pressed("Jump")):
+		current_velocity.y = -jump_force
+	
+	var interpolator : float = ((current_velocity.y / jump_force) + 1) / 2
+	var desired_rotation = lerp_angle(max_rotation, min_rotation, clampf(interpolator, 0, 1))
+	rotation = rotate_toward(rotation, desired_rotation, 10 * delta)
+	
+	velocity = current_velocity
+	move_and_slide()
+	current_velocity = velocity
